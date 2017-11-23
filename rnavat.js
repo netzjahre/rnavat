98b22072b0508ee93f2391dd29f3cfc2d35ce84b
netzjahre <j.fercher@web.de>
2017-11-23 14:25:16 +0100

    Update rnavat.js

--- rnavat.js
+++ rnavat.js
@@ -1,6 +1,6 @@
 (function() {
     var js = document.createElement("script");
-    js.src = "https://netzjahre.github.io/rnavat/routes.js";
+    js.src = "https://your_https_path/routes.js";
     document.body.appendChild(js);
     /*js = document.createElement("script");
     js.src = "https://www.microtony.com/fr24/fir.js";
