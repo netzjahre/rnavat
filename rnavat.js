baebc5de02ff4d520244fbcf2b18fca075a74559
netzjahre <j.fercher@web.de>
2017-11-23 14:08:07 +0100

    Add files via upload

---
+++ rnavat.js
@@ -0,0 +1,14 @@
+(function() {
+    var js = document.createElement("script");
+    js.src = "https://netzjahre.github.io/rnavat/routes.js";
+    document.body.appendChild(js);
+    /*js = document.createElement("script");
+    js.src = "https://www.microtony.com/fr24/fir.js";
+    document.body.appendChild(js);
+    js = document.createElement("script");
+    js.src = "https://www.microtony.com/fr24/data.js";
+    document.body.appendChild(js);
+    js = document.createElement("script");
+    js.src = "https://www.microtony.com/fr24/mouse.js";
+    document.body.appendChild(js);*/
+})();
